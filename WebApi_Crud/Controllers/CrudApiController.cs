67d08c8ef8420620a16bac9046040731590f0f30
Prashanth S <prashant77000@gmail.com>
2022-08-09 12:10:13 +0530

    Add project files.

---
+++ WebApi_Crud/Controllers/CrudApiController.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using WebApi_Crud.Models;
+
+namespace WebApi_Crud.Controllers
+{
+    public class CrudApiController : ApiController
+    {
+        crud_dbEntities db = new crud_dbEntities();
+
+        [System.Web.Http.HttpGet]
+        public IHttpActionResult GetEmployees()
+        {
+            List<Employee> listOfEmp = db.Employees.ToList();
+            return Ok(listOfEmp);
+        }
+        [System.Web.Http.HttpGet]
+        public IHttpActionResult GetEmployeeById(int id)
+        {
+            var emp = db.Employees.Where(model => model.id == id).FirstOrDefault();
+            return Ok(emp);
+        }
+
+
+        [System.Web.Http.HttpPost]
+        public IHttpActionResult EmpInsert(Employee e)
+        {
+            db.Employees.Add(e);
+            db.SaveChanges();
+            return Ok();
+        }
+
+        [System.Web.Http.HttpPut]
+        public IHttpActionResult EmpUpdate(Employee e)
+        {
+            db.Entry(e).State = System.Data.Entity.EntityState.Modified;
+            db.SaveChanges();
+ 
+            return Ok();
+
+        }
+
+        [System.Web.Http.HttpDelete]
+        public IHttpActionResult EmpDelete(int id)
+        {
+            var emp = db.Employees.Where(model => model.id == id).FirstOrDefault();
+            db.Entry(emp).State = System.Data.Entity.EntityState.Deleted;
+            db.SaveChanges();
+            return Ok();
+        }
+
+
+    }
+}
